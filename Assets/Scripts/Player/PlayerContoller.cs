224f3f95219150d7f3a9f451e17e29122d9743ad
VaVaT1m <v.t.v.04@mail.ru>
2024-10-12 14:36:44 +0300

    Удалил прыжок

--- Assets/Scripts/Player/PlayerContoller.cs
+++ Assets/Scripts/Player/PlayerContoller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    public float speed, jumpSpeed; //скорость перса, сила прыжка и чувствительность мыши, настраивай в инспекторе
+    public float speed; //скорость перса настраивай в инспекторе
     float vertical, horizontal, jump;
     bool isGrounded;
 
@@ -17,10 +17,6 @@ public class PlayerContoller : MonoBehaviour
     {
         vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        jump = Input.GetAxis("Jump") * Time.deltaTime * jumpSpeed;
-
-        if (isGrounded)
-        GetComponent<Rigidbody>().AddForce(transform.up * jump, ForceMode.Impulse);
 
         transform.Translate(new Vector3(horizontal, 0, vertical));
 
@@ -42,16 +38,5 @@ public class PlayerContoller : MonoBehaviour
                 currentItem = Instantiate(kanistra, itemSpawner);
                 return;
         }
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-            isGrounded = true;
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-            isGrounded = false;
-    }
+    } 
 }
